c2edf0ab46a7c0d7d506e230c0a938842d3c4716
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2026-06-27 21:47:28 +0530

    added volumes, changed deployment script

--- apps/Backend/src/index.ts
+++ apps/Backend/src/index.ts
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://tradeon.100xdc.com",
+      "https://tradeon.100xdc.com"],
     credentials: true,
   }));
 
